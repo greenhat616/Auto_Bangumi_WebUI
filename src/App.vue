af452c2e5cbb1b336d543a6ce00be912adf34a33
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2022-10-01 01:21:33 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
+import MenuBar from '@/components/MenuBar.vue';
 </script>
 
 <template>
   <Suspense>
-    <RouterView />
+    <div class="app">
+      <MenuBar />
+
+      <main>
+        <RouterView />
+      </main>
+    </div>
   </Suspense>
 </template>
 
 <style lang="scss">
 @import 'styles/base';
+
+.app {
+  display: flex;
+  height: 100vh;
+  background: #F0F0F0;
+
+  main {
+    flex: 1;
+  }
+}
 </style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,46 @@
+<script lang='ts' setup>
+const items = [
+  { icon: 'menu-unfold' },
+  { icon: 'home' },
+  { icon: 'calendar' },
+  { icon: 'play' },
+  { icon: 'download' },
+  { icon: 'setting-two' },
+  { icon: 'log' },
+]
+</script>
+
+<template>
+  <div class="menu-bar">
+
+    <template v-for="i in items">
+      <RouterLink
+        class="link"
+        to="home"
+      >
+        <iconpark-icon :name="i.icon" />
+      </RouterLink>
+    </template>
+  </div>
+</template>
+
+<style lang='scss' scope>
+.menu-bar {
+  width: 4.6875vw;
+  height: 100%;
+
+  @include flex(y);
+  gap: 2.9297vw;
+  padding: 1.9531vw 1.1719vw 1.1719vw;
+
+  background: $theme-color;
+  color: #fff;
+
+  @include fontSize(24);
+  text-align: center;
+
+  .link {
+    color: inherit;
+  }
+}
+</style>
